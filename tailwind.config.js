ca9f5de421b482df4d7ff8a875068faf47d8c0b4
Andrei G. Stanimir <astanimir@opcom.ro>
2023-09-29 15:55:33 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    fontFamily: {
+      sans: "Roboto Mono, monospace",
+    },
+    extend: {
+      height: {
+        screen: "100dvh",
+      },
+    },
+  },
+  plugins: [],
+};
